b98609e3a5503d6592478a1066dfa364b3ed0787
Martin Shopland <github@sleepistheenemy.co.uk>
2010-11-11 00:52:12 +0000

    Initial commit.

---
+++ Tool/Manifest.php
@@ -0,0 +1,13 @@
+<?php
+include 'AppDbProvider.php';
+
+class App_Tool_Manifest
+implements Zend_Tool_Framework_Manifest_ProviderManifestable
+{
+	public function getProviders()
+	{
+		return array(
+			new App_Tool_AppDbProvider()
+		);
+	}
+}
