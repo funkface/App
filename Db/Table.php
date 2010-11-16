0727a0c0de785be51d3b5b53a17a1de9d36efa85
Martin Shopland <martin.shopland@redbrickstudios.co.uk>
2010-11-16 18:28:04 +0000

    Loading member rows and rowsets based on new, normailised relation map format.

--- Db/Table.php
+++ Db/Table.php
@@ -9,14 +9,15 @@ class App_Db_Table extends Zend_Db_Table
     protected $_rowClass = 'App_Db_Table_Row';
 	protected $_rowsetClass = 'App_Db_Table_Rowset';
 	
-	public function getReferenceByRuleKey($ruleKey)
+	protected $_relationMap = array();
+	
+	public function getRelation($relName)
 	{
-		$refMap = $this->_getReferenceMapNormalized();
-        if(!isset($refMap[$ruleKey])) {
+        if(!isset($this->_relationMap[$relName])) {
             require_once "Zend/Db/Table/Exception.php";
-            throw new Zend_Db_Table_Exception("No reference rule \"$ruleKey\" from table $thisClass to table $tableClassname");
+            throw new Zend_Db_Table_Exception('No relation named ' . $relName);
         }
         
-        return $refMap[$ruleKey];
+        return $this->relationMap[$relName];
 	}
 }
--- Db/Table/Row.php
+++ Db/Table/Row.php
@@ -1,100 +1,160 @@
 <?php
 /**
- * 
+ *
  * @author Martin
  * @method App_Db_Table|null getTable() Returns the table object, or null if this is disconnected row
  */
 class App_Db_Table_Row extends Zend_Db_Table_Row
 {
-	
-	protected $_memberRowsets = array();
-	
-	public function __get($columnName)
-	{
-		$columnName = $this->_transformColumn($columnName);
-		
+    protected $_memberRows = array();
+    protected $_memberRowsets = array();
+
+    public function __get($columnName)
+    {
+        $columnName = $this->_transformColumn($columnName);
+
         if(array_key_exists($columnName, $this->_data))
         {
-        	return $this->_data[$columnName];
+            return $this->_data[$columnName];
         }
+
+        $rel = $this->getTable()->getRelation($columnName);
         
-        return $this->_getMemberRowset($columnName);
-	}
-	
-	/*
-	 * @returns bool
-	 */
-	public function isModified()
-	{
-		if(count($this->_modifiedFields) > 0)
-		{
-			return true;
-		}
-		
-		foreach($this->_memberRowsets as $rowSet)
-		{
-			if($rowSet->isModified())
-			{
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public function save()
-	{
-		if($this->isModified()){
-			foreach($this->_memberRowsets as $rowSet)
-			{
-				$rowSet->save();
-			}
-			
-			return parent::save();
-		}
-		
-		$primaryKey = $this->_getPrimaryKey(true);
+        switch($rel['relation'])
+        {
+            case 0: // has one
+                return $this->_getMemberRow($columnName);
+                
+            case 1: // has many
+            case 2: // many to many
+                return $this->_getMemberRowset($columnName);
+
+            default:
+                throw new Zend_Db_Table_Row_Exception('Weird relation type' . $rel['relation']);
+        }
+
+    }
+
+    /*
+     * @returns bool
+     */
+    public function isModified()
+    {
+        if(count($this->_modifiedFields) > 0)
+        {
+            return true;
+        }
+
+        foreach($this->_memberRowsets as $rowSet)
+        {
+            if($rowSet->isModified())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function save()
+    {
+        if($this->isModified()){
+            
+            foreach($this->_memberRows as $row)
+            {
+                $row->save();
+            }
+            
+            $result = parent::save();
+            
+            foreach($this->_memberRowsets as $rowSet)
+            {
+                $rowSet->save();
+            }
+            	
+            return $result;
+        }
+
+        $primaryKey = $this->_getPrimaryKey(true);
         if (count($primaryKey) == 1) {
             return current($primaryKey);
         }
 
         return $primaryKey;
-	}
-	
-	protected function _getMemberRowset($memberName)
-	{
-		if(!isset($this->_memberRowsets[$memberName]))
-		{
-			$this->loadMemberRowset($memberName);
-		}
-		
-		return $this->_memberRowsets[$memberName];
-	}
-	
-	/**
-	 * 
-	 * @param $memberName
-	 * @param $select
-	 * @return App_Db_Table_Rowset
-	 */
-	public function loadMemberRowset($memberName, Zend_Db_Table_Select $select = null)
-	{
-	    $ref = $this->getTable()->getReferenceByRuleKey($memberName);
-        $this->_memberRowsets[$memberName] = 
-            $this->findDependentRowset($ref['refTableClass'], $memberName, $select);
-            
+    }
+
+    protected function _getMemberRow($memberName)
+    {
+        if(!isset($this->_memberRows[$memberName]))
+        {
+            $this->loadMemberRow($memberName);
+        }
+
+        return $this->_memberRows[$memberName];
+    }
+
+    protected function _getMemberRowset($memberName)
+    {
+        if(!isset($this->_memberRowsets[$memberName]))
+        {
+            $this->loadMemberRowset($memberName);
+        }
+
         return $this->_memberRowsets[$memberName];
-	}
-	
-	/*
-	
-	   $this->findParentRow($tableClass, $ruleName); // has one
-	   $this->findDependentRowset($tableClass, $ruleName, $select); // has many
-	   $this->findManyToManyRowset($tableClass, $intTableClass, $rule1, $rule2, $select); // many to many
-	
-	
-	 */
-	
-	
-	
+    }
+
+    /**
+     * 
+     * @param string $memberName
+     * @return App_Db_Table_Row
+     */
+    public function loadMemberRow($memberName)
+    {
+        $rel = $this->getTable()->getRelation($memberName);
+
+        switch($rel['relation'])
+        {
+            case 0: // has one
+                $this->_memberRows[$memberName] =
+                    $this->findParentRow($rel['refTableClass'], $rel['rules'][0]);
+                break;
+
+            default:
+                throw new Zend_Db_Table_Row_Exception($memberName . ' is not a row member');
+        }
+
+        return $this->_memberRows[$memberName];
+    }
+
+    /**
+     *
+     * @param string $memberName
+     * @param Zend_Db_Table_Select $select
+     * @return App_Db_Table_Rowset
+     */
+    public function loadMemberRowset($memberName, Zend_Db_Table_Select $select = null)
+    {
+        $rel = $this->getTable()->getRelation($memberName);
+         
+        switch($rel['relation'])
+        {
+            case 1: // has many
+                $this->_memberRowsets[$memberName] =
+                    $this->findDependentRowset($rel['refTableClass'], $rel['rules'][0], $select);
+                break;
+
+            case 2: // many to many
+                $this->_memberRowsets[$memberName] = $this->findManyToManyRowset(
+                    $rel['refTableClass'], $rel['intTableClass'],
+                    $rel['rules'][0], $rel['rules'][1],
+                    $select
+                );
+                break;
+                 
+            default:
+                throw new Zend_Db_Table_Row_Exception($memberName . ' is not a rowset member');
+        }
+
+        return $this->_memberRowsets[$memberName];
+    }
 }
